bb19cb5bde84a557ca94410f2befecf7d1e6c0a7
Мирослав Левдиков <deadstronghold@gmail.com>
2020-10-04 20:39:09 +0300

    002

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -33,11 +35,11 @@ public class Main {
 
                 if (!generalTrinagle.trianglesList.isEmpty()) {
                     generalTrinagle.save("General_Triangles_Reserve" + formatter.format(date) + ".txt");
-                    System.out.println("Reserve copy for general triangles is created;");
+                    System.out.println("Reserve copy for general triangles has been created;");
                 }
                 if (!equilateral_triangles.equilTrianglesList.isEmpty()) {
                     equilateral_triangles.save("Eq_Triangles_Reserve" + formatter.format(date) + ".txt");
-                    System.out.println("Reserve copy for equilateral triangles is created;");
+                    System.out.println("Reserve copy for equilateral triangles has been created;");
                 }
 
                 generalTrinagle.trianglesList.removeIf(triangle -> !triangle.check_existence());
@@ -76,34 +78,61 @@ public class Main {
                 generalTrinagle.save("General_Triangles.txt");
                 break;
             case "N":
-                System.out.println("Press 'Y' if you want to work with general triangles list, press 'N' if you want to skip.");
-                Scanner scanner = new Scanner(System.in);
-                String chosenVariant2 = scanner.nextLine().toUpperCase();
-                switch (chosenVariant2) {
-                    case "Y" :
-                        System.out.println("Enter the name of file");
-                        Scanner scanner1 = new Scanner(System.in);
-                        String filename1 = scanner1.nextLine();
-                        generalTrinagle.load(filename1);
+                System.out.println("Press 'Y' if you want to load reserve copy, press 'N' if you want to choose another file.");
+                Scanner scanner4 = new Scanner(System.in);
+                String chosenVariant3 = scanner4.nextLine().toUpperCase();
+                switch (chosenVariant3) {
+                    case "Y":
+                        String filePath = "E:\\Users\\Мирослав Левдиков\\IdeaProjects\\Lab_3";
+                        File directory = new File(filePath);
+                        String[] reserveFiles = directory.list(new FilenameFilter() {
+                            @Override
+                            public boolean accept(File dir, String name) {
+                                return name.contains("Reserve");
+                            }
+                        });
+                        System.out.println("The reserve copy list:");
+                        for (int i = 0; i < reserveFiles.length; i++) {
+                            System.out.println((i) + " " + reserveFiles[i]);
+                        }
+                        System.out.println("Enter the number of copy you want to load");
+                        Scanner scanner5 = new Scanner(System.in);
+                        String chosenCopy = scanner5.nextLine();
+                        generalTrinagle.load(reserveFiles[Integer.parseInt(chosenCopy)]);
                         System.out.println(generalTrinagle);
-                        generalTrinagle.save("Save_g_1");
-                    case "N" :
-                        System.out.println("Press 'Y' if you want to work with equilateral triangles list, press 'N' if you want to skip.");
-                        Scanner scanner2 = new Scanner(System.in);
-                        String chosenVariant3 = scanner2.nextLine().toUpperCase();
-                        switch (chosenVariant3) {
+                        break;
+                    case "N":
+                        System.out.println("Press 'Y' if you want to work with general triangles list, press 'N' if you want to skip.");
+                        Scanner scanner = new Scanner(System.in);
+                        String chosenVariant2 = scanner.nextLine().toUpperCase();
+                        switch (chosenVariant2) {
                             case "Y":
                                 System.out.println("Enter the name of file");
-                                Scanner scanner3 = new Scanner(System.in);
-                                String filename2 = scanner3.nextLine();
-                                equilateral_triangles.load(filename2);
-                                System.out.println(equilateral_triangles);
-                                equilateral_triangles.save("Save_eq_1");
+                                Scanner scanner1 = new Scanner(System.in);
+                                String filename1 = scanner1.nextLine();
+                                generalTrinagle.load(filename1);
+                                System.out.println(generalTrinagle);
+                                generalTrinagle.save("Save_g_1");
                             case "N":
-                                System.out.println("Shut down");
+                                System.out.println("Press 'Y' if you want to work with equilateral triangles list, press 'N' if you want to skip.");
+                                Scanner scanner2 = new Scanner(System.in);
+                                String chosenVariant4 = scanner2.nextLine().toUpperCase();
+                                switch (chosenVariant4) {
+                                    case "Y":
+                                        System.out.println("Enter the name of file");
+                                        Scanner scanner3 = new Scanner(System.in);
+                                        String filename2 = scanner3.nextLine();
+                                        equilateral_triangles.load(filename2);
+                                        System.out.println(equilateral_triangles);
+                                        equilateral_triangles.save("Save_eq_1");
+                                    case "N":
+                                        System.out.println("Shut down");
+                                        break;
+                                }
                                 break;
                         }
                         break;
+
                 }
                 break;
             default:
