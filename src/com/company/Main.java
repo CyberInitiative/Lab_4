ac353b8afedd27f9e64f10b28c5f0d89b1220204
Мирослав Левдиков <deadstronghold@gmail.com>
2020-10-06 19:11:25 +0300

    003

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
         TriangleDB generalTrinagle = new TriangleDB();
         EquliteralDB equilateral_triangles = new EquliteralDB();
 
-        System.out.println("Hello! Please, enter 'Y' if you want to work with current DB or enter 'N' if you want to choose another one.");
+        System.out.println("Hello! Please, enter 'Y' if you want to work with current DB or enter 'N' if you want to choose another one;");
 
         Scanner input = new Scanner(System.in);
         String chosenVariant1 = input.nextLine().toUpperCase();
@@ -71,14 +71,14 @@ public class Main {
                     EquilateralTriangle equilateral_triangle = Collections.min(equilateral_triangles.equilTrianglesList, Comparator.comparing(s -> s.find_median()));
                     System.out.println("The lowest value among medians: " + equilateral_triangle.find_median());
                 } else {
-                    System.out.println("No equilateral triangles exist");
+                    System.out.println("No equilateral triangles exist.");
                 }
 
                 equilateral_triangles.save("Eq_Triangles.txt");
                 generalTrinagle.save("General_Triangles.txt");
                 break;
             case "N":
-                System.out.println("Press 'Y' if you want to load reserve copy, press 'N' if you want to choose another file.");
+                System.out.println("Press 'Y' if you want to load reserve copy, press 'N' if you want to choose another file;");
                 Scanner scanner4 = new Scanner(System.in);
                 String chosenVariant3 = scanner4.nextLine().toUpperCase();
                 switch (chosenVariant3) {
@@ -91,30 +91,35 @@ public class Main {
                                 return name.contains("Reserve");
                             }
                         });
-                        System.out.println("The reserve copy list:");
-                        for (int i = 0; i < reserveFiles.length; i++) {
-                            System.out.println((i) + " " + reserveFiles[i]);
+                        if(reserveFiles != null && reserveFiles.length > 0)
+                        {
+                            System.out.println("The reserve copy list:");
+                            for (int i = 0; i < reserveFiles.length; i++) {
+                                System.out.println((i) + " " + reserveFiles[i]);
+                            }
+                            System.out.println("Enter the number of copy you want to load;");
+                            Scanner scanner5 = new Scanner(System.in);
+                            String chosenCopy = scanner5.nextLine();
+                            generalTrinagle.load(reserveFiles[Integer.parseInt(chosenCopy)]);
+                            System.out.println(generalTrinagle);
                         }
-                        System.out.println("Enter the number of copy you want to load");
-                        Scanner scanner5 = new Scanner(System.in);
-                        String chosenCopy = scanner5.nextLine();
-                        generalTrinagle.load(reserveFiles[Integer.parseInt(chosenCopy)]);
-                        System.out.println(generalTrinagle);
+                        else
+                            System.out.println("There are no backups in the folder.");
                         break;
                     case "N":
-                        System.out.println("Press 'Y' if you want to work with general triangles list, press 'N' if you want to skip.");
+                        System.out.println("Press 'Y' if you want to work with general triangles list, press 'N' if you want to skip;");
                         Scanner scanner = new Scanner(System.in);
                         String chosenVariant2 = scanner.nextLine().toUpperCase();
                         switch (chosenVariant2) {
                             case "Y":
-                                System.out.println("Enter the name of file");
+                                System.out.println("Enter the name of file;");
                                 Scanner scanner1 = new Scanner(System.in);
                                 String filename1 = scanner1.nextLine();
                                 generalTrinagle.load(filename1);
                                 System.out.println(generalTrinagle);
                                 generalTrinagle.save("Save_g_1");
                             case "N":
-                                System.out.println("Press 'Y' if you want to work with equilateral triangles list, press 'N' if you want to skip.");
+                                System.out.println("Press 'Y' if you want to work with equilateral triangles list, press 'N' if you want to skip;");
                                 Scanner scanner2 = new Scanner(System.in);
                                 String chosenVariant4 = scanner2.nextLine().toUpperCase();
                                 switch (chosenVariant4) {
@@ -126,17 +131,22 @@ public class Main {
                                         System.out.println(equilateral_triangles);
                                         equilateral_triangles.save("Save_eq_1");
                                     case "N":
-                                        System.out.println("Shut down");
+                                        System.out.println("Shut down.");
+                                        break;
+                                    default:
+                                        System.out.println("Error, wrong symbol. Shut down.");
                                         break;
                                 }
                                 break;
                         }
                         break;
-
+                    default:
+                        System.out.println("Error, wrong symbol. Shut down.");
+                        break;
                 }
                 break;
             default:
-                System.out.println("Error. Shut down;");
+                System.out.println("Error, wrong symbol. Shut down.");
                 break;
         }
     }
